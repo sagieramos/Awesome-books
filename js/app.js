56deb08186818056ab115d62d242821629c68de3
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-08-08 11:29:02 +0300

    code optimized

--- js/app.js
+++ js/app.js
@@ -8,24 +8,21 @@ book.display(hero);
 
 function handleBookStorage(e) {
   const { target } = e;
+  e.preventDefault();
   if (target.matches('.remove')) {
     const books = [...(document.querySelectorAll('.remove'))];
     const i = books.indexOf(target);
     book.delete(i);
     book.display(hero);
   }
+  else if (target.matches('#submit')) 
+  {  const formData = new FormData(form);
+     const title = formData.get('title');
+     const author = formData.get('author');
+           book.store(title, author);
+      form.reset(); 
+      book.display(hero); }
 }
 
-function handleSubmit(e) {
-  e.preventDefault();
-  const formData = new FormData(form);
-  const title = formData.get('title');
-  const author = formData.get('author');
 
-  book.store(title, author);
-  form.reset();
-  book.display(hero);
-}
-
-window.addEventListener('click', handleBookStorage);
-form.addEventListener('submit', handleSubmit);
+window.addEventListener('click',(e)=> handleBookStorage(e));
